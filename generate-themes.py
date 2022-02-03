46dc3f05d7111d94e742446e592665e8fbef8eed
joka42 <jas.nombre@web.de>
2022-02-03 12:36:00 +0100

    Tela theme added

--- generate-themes.py
+++ generate-themes.py
@@ -75,90 +75,96 @@ os.system("./build-themes.py")
 os.chdir(curdir)
 
 # Mint-Y color variations
+print(y_hex_colors1.keys())
 for color in y_hex_colors1.keys():
     for variant in ["", "-Dark"]:
         original_name = "Mint-Y%s" % variant
         path = os.path.join("src/Mint-Y/variations/%s" % color)
-        if os.path.isdir(path):
-            print("Derivating %s-%s" % (original_name, color))
 
-            # Copy theme
-            theme = "usr/share/themes/%s-%s" % (original_name, color)
-            theme_index = os.path.join(theme, "index.theme")
-            os.system("cp -R usr/share/themes/%s %s" % (original_name, theme))
+        if not os.path.isdir(path):
+            print("Directory %s not found." % path)
+            continue
+        
+        print("Derivating %s-%s" % (original_name, color))
 
-            # Theme name
-            for key in ["Name", "GtkTheme"]:
-                change_value(key, "%s-%s" % (original_name, color), theme_index)
+        # Copy theme
+        theme = "usr/share/themes/%s-%s" % (original_name, color)
+        theme_index = os.path.join(theme, "index.theme")
+        print("cp -R usr/share/themes/%s %s" % (original_name, theme))
+        os.system("cp -R usr/share/themes/%s %s" % (original_name, theme))
 
-            for key in ["IconTheme"]:
-                change_value(key, "%s-%s" % (original_name, color), theme_index)
+        # Theme name
+        for key in ["Name", "GtkTheme"]:
+            change_value(key, "%s-%s" % (original_name, color), theme_index)
 
-            # Regenerate GTK3 sass
-            os.system("cp -R src/Mint-Y/gtk-3.0/sass %s/gtk-3.0/" % theme)
-            y_colorize_directory("%s/gtk-3.0/sass" % theme, color)
-            os.chdir("%s/gtk-3.0" % theme)
-            # os.system("sed -i 's/no-tint/tint/gI' ./sass/gtk.scss")
-            # os.system("sed -i 's/no-tint/tint/gI' ./sass/gtk-dark.scss")
-            if (variant == "-Dark"):
-                os.system("cp sass/gtk-dark.scss sass/gtk.scss")
-                os.system("sassc ./sass/gtk.scss gtk.css")
-            else:
-                os.system("sassc ./sass/gtk-dark.scss gtk-dark.css")
-                os.system("sassc ./sass/gtk.scss gtk.css")
+        for key in ["IconTheme"]:
+            change_value(key, "%s-%s" % (original_name, color), theme_index)
 
-            os.system("rm -rf sass .sass-cache")
-            os.chdir(curdir)
+        # Regenerate GTK3 sass
+        os.system("cp -R src/Mint-Y/gtk-3.0/sass %s/gtk-3.0/" % theme)
+        y_colorize_directory("%s/gtk-3.0/sass" % theme, color)
+        os.chdir("%s/gtk-3.0" % theme)
+        # os.system("sed -i 's/no-tint/tint/gI' ./sass/gtk.scss")
+        # os.system("sed -i 's/no-tint/tint/gI' ./sass/gtk-dark.scss")
+        if (variant == "-Dark"):
+            os.system("cp sass/gtk-dark.scss sass/gtk.scss")
+            os.system("sassc ./sass/gtk.scss gtk.css")
+        else:
+            os.system("sassc ./sass/gtk-dark.scss gtk-dark.css")
+            os.system("sassc ./sass/gtk.scss gtk.css")
 
-            # Regenerate Cinnamon sass
-            os.system("cp -R src/Mint-Y/cinnamon/sass %s/cinnamon/" % theme)
-            y_colorize_directory("%s/cinnamon/sass" % theme, color)
-            os.chdir("%s/cinnamon" % theme)
-            if (variant == "-Dark"):
-                os.system("cp sass/cinnamon-dark.scss sass/cinnamon.scss")
-            os.system("sassc ./sass/cinnamon.scss cinnamon.css")
-            os.system("rm -rf sass .sass-cache")
-            os.chdir(curdir)
+        os.system("rm -rf sass .sass-cache")
+        os.chdir(curdir)
 
-            # Accent color
-            files = []
-            files.append(os.path.join(theme, "gtk-2.0", "gtkrc"))
-            files.append(os.path.join(theme, "gtk-2.0", "main.rc"))
-            files.append(os.path.join(theme, "gtk-2.0", "panel.rc"))
-            files.append(os.path.join(theme, "gtk-2.0", "apps.rc"))
-            files.append(os.path.join(theme, "gtk-2.0", "menubar-toolbar.rc"))
-            for file in files:
-                if os.path.exists(file):
-                    for accent in Y_HEX_ACCENT1:
-                        os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors1[color], 'file': file})
-                    for accent in Y_HEX_ACCENT2:
-                        os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors2[color], 'file': file})
-                    for accent in Y_HEX_ACCENT3:
-                        os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors3[color], 'file': file})
-                    for accent in Y_HEX_ACCENT4:
-                        os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors4[color], 'file': file})
+        # Regenerate Cinnamon sass
+        os.system("cp -R src/Mint-Y/cinnamon/sass %s/cinnamon/" % theme)
+        y_colorize_directory("%s/cinnamon/sass" % theme, color)
+        os.chdir("%s/cinnamon" % theme)
+        if (variant == "-Dark"):
+            os.system("cp sass/cinnamon-dark.scss sass/cinnamon.scss")
+        os.system("sassc ./sass/cinnamon.scss cinnamon.css")
+        os.system("rm -rf sass .sass-cache")
+        os.chdir(curdir)
 
-            # Remove metacity-theme-3.xml (it doesn't need to be derived since it's using GTK colors, and Cinnamon doesn't want to list it)
-            os.system("rm -f %s" % os.path.join(theme, "metacity-1", "metacity-theme-3.xml"))
+        # Accent color
+        files = []
+        files.append(os.path.join(theme, "gtk-2.0", "gtkrc"))
+        files.append(os.path.join(theme, "gtk-2.0", "main.rc"))
+        files.append(os.path.join(theme, "gtk-2.0", "panel.rc"))
+        files.append(os.path.join(theme, "gtk-2.0", "apps.rc"))
+        files.append(os.path.join(theme, "gtk-2.0", "menubar-toolbar.rc"))
+        for file in files:
+            if os.path.exists(file):
+                for accent in Y_HEX_ACCENT1:
+                    os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors1[color], 'file': file})
+                for accent in Y_HEX_ACCENT2:
+                    os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors2[color], 'file': file})
+                for accent in Y_HEX_ACCENT3:
+                    os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors3[color], 'file': file})
+                for accent in Y_HEX_ACCENT4:
+                    os.system("sed -i s'/%(accent)s/%(color_accent)s/gI' %(file)s" % {'accent': accent, 'color_accent': y_hex_colors4[color], 'file': file})
 
-            directories = []
-            directories.append(os.path.join(theme, "cinnamon/common-assets"))
-            directories.append(os.path.join(theme, "cinnamon/light-assets"))
-            directories.append(os.path.join(theme, "cinnamon/dark-assets"))
-            for directory in directories:
-                if os.path.exists(directory):
-                    y_colorize_directory(directory, color)
+        # Remove metacity-theme-3.xml (it doesn't need to be derived since it's using GTK colors, and Cinnamon doesn't want to list it)
+        os.system("rm -f %s" % os.path.join(theme, "metacity-1", "metacity-theme-3.xml"))
 
-            # Assets
-            os.system("rm -rf %s/gtk-3.0/assets" % theme)
-            os.system("rm -rf %s/gtk-2.0/assets" % theme)
-            if variant == "-Dark":
-                os.system("cp -R %s/gtk-2.0/assets-dark %s/gtk-2.0/assets" % (path, theme))
-                os.system("cp -R %s/xfwm4-dark/*.png %s/xfwm4/" % (path, theme))
-            else:
-                os.system("cp -R %s/gtk-2.0/assets %s/gtk-2.0/assets" % (path, theme))
-                os.system("cp -R %s/xfwm4/*.png %s/xfwm4/" % (path, theme))
-            os.system("cp -R %s/gtk-3.0/assets %s/gtk-3.0/assets" % (path, theme))
+        directories = []
+        directories.append(os.path.join(theme, "cinnamon/common-assets"))
+        directories.append(os.path.join(theme, "cinnamon/light-assets"))
+        directories.append(os.path.join(theme, "cinnamon/dark-assets"))
+        for directory in directories:
+            if os.path.exists(directory):
+                y_colorize_directory(directory, color)
+
+        # Assets
+        os.system("rm -rf %s/gtk-3.0/assets" % theme)
+        os.system("rm -rf %s/gtk-2.0/assets" % theme)
+        if variant == "-Dark":
+            os.system("cp -R %s/gtk-2.0/assets-dark %s/gtk-2.0/assets" % (path, theme))
+            os.system("cp -R %s/xfwm4-dark/*.png %s/xfwm4/" % (path, theme))
+        else:
+            os.system("cp -R %s/gtk-2.0/assets %s/gtk-2.0/assets" % (path, theme))
+            os.system("cp -R %s/xfwm4/*.png %s/xfwm4/" % (path, theme))
+        os.system("cp -R %s/gtk-3.0/assets %s/gtk-3.0/assets" % (path, theme))
 
 
 # Files
